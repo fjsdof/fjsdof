5cfbce1785ccf8167fba1b13bac487eda999f8d5
fjsdof <647692579@qq.com>
2024-10-22 22:01:53 +0800

    =)

--- password/The rail fence.c
+++ password/The rail fence.c
@@ -1,26 +1,61 @@
 #include <stdio.h>
-#define N 6       /*栏数*/
-#define M 100
+#define N 10000
 int main()
 {
-    int X=0,Y=0;
-    char ch1[M]="\0",ch2[M]="\0";
+    int X=0,Y=0,M;
+    char ch1[N]="\0",ch2[N]="\0";
     printf("请输入:\n");
-    scanf("%s",ch1);
-    for (int i=0;ch1[i]!=0;i++)
-    X++;
-    if (X%N==0)
-    Y=X/N;
-    else
-    Y=(X-X%N)/N+1;
-    int i=0;
-    for (int j=0;j<N;j++)
+    gets(ch1);
+    printf("请输入栏数:\n");
+    scanf("%d",&M);
+    if (M>=0)
     {
-        for (i=i;i<Y*(j+1);i++)
-        ch2[i]=ch1[j+N*(i%Y)];
+        for (int i=0;ch1[i]!=0;i++)
+        X++;
+        if (X%M==0)
+        Y=X/M;
+        else
+        {
+            Y=(X-X%M)/M+1;
+            for (int i=X;i<Y*M;i++)
+            ch1[i]=' ';
+            Y++;
+        }
+        int i=0;
+        for (int j=0;j<M;j++)
+        {
+            for (i=i;i<Y*(j+1);i++)
+            ch2[i]=ch1[j+M*(i%Y)];
+        }
+        printf("加密:\n{");
+        for (int i=0;i<M*Y;i++)
+        printf("%c",ch2[i]);
+        printf("}");
     }
-    printf("加密:\n");
-    for (int i=0;i<N*Y;i++)
-    printf("%c",ch2[i]);
+    else
+    {
+        for (int i=0;ch1[i]!=0;i++)
+        X++;
+        M=-M;
+        if (X%M==0)
+        Y=X/M;
+        else
+        {
+            Y=(X-X%M)/M+1;
+            for (int i=X;i<Y*M;i++)
+            ch1[i]=' ';
+            Y++;
+        }
+        int i=0;
+        for (int j=0;j<Y;j++)
+        {
+            for (i=i;i<M*(j+1);i++)
+            ch2[i]=ch1[j+Y*(i%M)];
+        }
+        printf("解密:\n{");
+        for (int i=0;i<M*Y;i++)
+        printf("%c",ch2[i]);
+        printf("}");
+    }
     return 0;
 }
