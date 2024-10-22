aa9698e59efc785de75798a6128c085bd6b21f64
fjsdof <647692579@qq.com>
2024-10-22 08:44:57 +0800

    =)

---
+++ password/caesar.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define N 100
+int main()
+{
+    int num,NUM,X=0;
+    char ch1[N],ch2[N];
+    printf("请输入:\n");
+    scanf("%s",ch1);
+    printf("位数:\n");
+    scanf("%d",&num);
+    NUM=num%26;
+    for (int i=0;ch1[i]!='\0';i++)
+    {
+        if (ch1[i]>='A'&&ch1[i]<='Z')
+        {
+            if (ch1[i]>'Z'-NUM)
+            ch2[i]=ch1[i]+NUM-26;
+            else
+            ch2[i]=ch1[i]+NUM;
+        }
+        else if (ch1[i]>='a'&&ch1[i]<='z')
+        {
+            if (ch1[i]>'z'-NUM)
+            ch2[i]=ch1[i]+NUM-26;
+            else
+            ch2[i]=ch1[i]+NUM;
+        }
+        else
+        ch2[i]=ch1[i];
+        X++;
+    }
+    printf("加密:\n");
+    for (int i=0;i<X+1;i++)
+    printf("%c",ch2[i]);
+    getchar();
+    getchar();
+    return 0;
+}
